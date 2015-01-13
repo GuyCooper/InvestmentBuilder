87b126497231020c07c3e680c3fa8548268c0832
GuyCooper <guy@guycooper.plus.com>
2015-01-13 21:59:00 +0000

    Refactored into common library

---
+++ InvestmentBuilderConsole/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestmentBuilderConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine(@"syntax: InvestmentBuilder <-p:path> <-t> <-v:Valuation Date> <-d:database string>. where -t = test -p = path and -d specifies valuation date");
+                return;
+            }
+
+            string path = @"C:\Users\Guy\Documents\Guy\Investments\Investment Club\accounts\";
+            string connectionsstr = string.Empty;
+            bool bTest = false;
+            DateTime? dtValuationDate = null;
+
+            foreach (var arg in args)
+            {
+                if (arg[0] == '-')
+                {
+                    switch (arg[1])
+                    {
+                        case 'p':
+                            if (arg[2] == ':')
+                                path = arg.Substring(3);
+                            break;
+                        case 't':
+                            bTest = true;
+                            break;
+                        case 'v':
+                            if (arg[2] == ':')
+                                dtValuationDate = DateTime.Parse(arg.Substring(3));
+                            break;
+                        case 'd':
+                            if (arg[2] == ':')
+                                connectionsstr = arg.Substring(3);
+                            break;
+                    }
+                }
+            }
+
+            if (path.Last() != '\\')
+            {
+                path = path + "\\";
+            }
+
+            //load in any trades from the trades file
+            string tradeFile = string.Format("{0}Trades.xml", path);
+
+            InvestmentBuilder.AssetSheetBuilder.BuildAssetSheet(tradeFile, path, connectionsstr, bTest, dtValuationDate.Value,
+                                                                    InvestmentBuilder.DataFormat.EXCEL);
+
+        }
+    }
+}
---
+++ InvestmentBuilderLib/AssetSheetBuilder.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestmentBuilder
+{
+    public enum DataFormat
+    {
+        EXCEL,
+        DATABASE
+    }
+
+    public static class AssetSheetBuilder
+    {
+        /// <summary>
+        /// factory method to create the correct factory class
+        /// </summary>
+        /// <param name="format"></param>
+        /// <param name="path"></param>
+        /// <param name="connectionstr"></param>
+        /// <param name="bTest"></param>
+        /// <returns></returns>
+        private static IInvestmentFactory BuildFactory(DataFormat format, string path, string connectionstr, bool bTest)
+        {
+            switch(format)
+            {
+                case DataFormat.EXCEL:
+                    return new ExcelInvestmentFactory(path, bTest);
+                    break;
+                case DataFormat.DATABASE:
+                    return new DatabaseInvestmentFactory(path, connectionstr, bTest);
+                    break;
+            }
+
+            throw new ArgumentException("invalid dataformat");
+             
+        }
+          /// <summary>
+        /// build asset sheet for current month
+        /// </summary>
+        /// <param name="bTest"></param>
+        /// <param name="path"></param>
+        public static void BuildAssetSheet(string tradeFile, string path, string connectionstr, bool bTest, DateTime valuationDate, DataFormat format)
+        {
+            var factory = BuildFactory(format, path, connectionstr, bTest);
+            try
+            {
+                var trades = TradeLoader.GetTrades(tradeFile);
+
+                var recordBuilder = factory.CreateInvestmentRecordBuilder();
+                var dataReader = factory.CreateCompanyDataReader();
+                var assetWriter = factory.CreateAssetStatementWriter();
+                var cashAccountReader = factory.CreateCashAccountReader();
+
+                var dtPreviousValuation = cashAccountReader.GetPreviousValuationDate();
+                //first extract the cash account data
+                var cashAccountData = cashAccountReader.GetCashAccountData(valuationDate, dtPreviousValuation);
+                //parse the trade file for any trades for this month and update the investment record
+                //var trades = TradeLoader.GetTrades(tradeFile);
+                recordBuilder.BuildInvestmentRecords(trades, cashAccountData, valuationDate);
+
+                //now extract the latest data from the investment record
+                var lstData = dataReader.GetCompanyData(valuationDate, dtPreviousValuation).ToList();
+                foreach (var val in lstData)
+                {
+                    Console.WriteLine("{0} : {1} : {2} : {3} : {4}", val.sName, val.dSharePrice, val.dNetSellingValue, val.dMonthChange, val.dMonthChangeRatio);
+                }
+
+                //finally, build the asset statement
+                assetWriter.WriteAssetStatement(lstData, cashAccountData, dtPreviousValuation, valuationDate);
+
+                factory.CommitData();
+            }
+            finally
+            {
+                //app.Workbooks.Close();
+                factory.Close();
+            }
+        }
+    }
+}
