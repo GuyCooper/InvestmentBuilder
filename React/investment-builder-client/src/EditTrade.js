a06b680ccaa7028e2e10d85643d4d05306f9e66f
Guy Cooper <guy@guycooper.plus.com>
2021-10-21 21:41:29 +0100

    imlemented EditTrade modal.

--- React/investment-builder-client/src/EditTrade.js
+++ React/investment-builder-client/src/EditTrade.js
@@ -1,10 +1,23 @@
-import React,  { useState, useRef } from 'react';
-import { Modal } from 'react-bootstrap';
+import React,  { useState } from 'react';
+import { Button, ButtonGroup, Modal, ToggleButton } from 'react-bootstrap';
+import {Form} from 'react-bootstrap';
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css'
 
 const EditTrade = function(props)  {
 
-    let onOk = function() {
-        console.log('user clicked ok!');
+    const BUY = '1';
+    const SELL = '2';
+    const OTHER = '3';
+
+    const [selectedDate, setSelectedDate] = useState( new Date());
+    const [selectedAction, setSelectedAction] = useState(BUY);
+    const [quantity, setQuantity] = useState('');
+
+    let handleSubmit = function() {    
+        console.log( 'selected data ' + selectedDate);
+        console.log( 'selected action ' + selectedAction);
+        console.log( 'selected quantity ' + quantity);
         props.onHide();
     };
 
@@ -13,20 +26,73 @@ const EditTrade = function(props)  {
             size="lg"
             show={props.show}
             aria-labelledby="contained-modal-title-vcenter"
-            centered>
+            centered
+            onHide={props.onHide}>
             <Modal.Header closeButton>
                 <Modal.Title
                     id="contained-modal-title-vcenter">
-                        {props.name}
+                        Edit Trade
                 </Modal.Title>                
             </Modal.Header>
             <Modal.Body>
-                <p>add some stuff here...</p>
+                <h2>{props.name}</h2>
+                <br/>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group>
+                        <Form.Label>Transaction Date</Form.Label>
+                        <DatePicker
+                            selected={selectedDate}
+                            onChange={ (date) => setSelectedDate( date )}                            
+                            className="form-control"
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Action</Form.Label>
+                        <div key={'inline-radio'}>
+                            <Form.Check 
+                                inline
+                                label="Buy"
+                                name="tradeAction"
+                                type="radio"
+                                defaultChecked={selectedAction === BUY }
+                                onChange={(e) => setSelectedAction(BUY)}
+                            />
+                            <Form.Check 
+                                inline
+                                label="Sell"
+                                name="tradeAction"
+                                type="radio"
+                                defaultChecked={selectedAction === SELL}                                
+                                onChange={(e) => setSelectedAction(SELL)}
+                            />
+                            <Form.Check 
+                                inline
+                                label="Other"
+                                name="tradeAction"
+                                type="radio"
+                                defaultChecked={selectedAction === OTHER}
+                                onChange={(e) => setSelectedAction(OTHER)}
+                            />                                                        
+                        </div>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Quantity</Form.Label>
+                        <Form.Control 
+                                type="number" 
+                                value={quantity} 
+                                onChange={(e) => setQuantity(e.currentTarget.value) } 
+                                placeholder="Amount" />
+                    </Form.Group>    
+                    <br/>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
             </Modal.Body>
-            <Modal.Footer>
-                <button onClick={onOk} >OK</button>
+            {/* <Modal.Footer>
+                <button variant="primary" onClick={onOk} >OK</button>
                 <button onClick={props.onHide}   >Cancel</button>
-            </Modal.Footer>
+            </Modal.Footer> */}
         </Modal>
     );
 
