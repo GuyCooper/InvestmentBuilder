98e341982d07eea536a374a2af444289785cfe1c
Guy Cooper <guy@guycooper.plus.com>
2024-04-28 20:20:49 +0100

    upgrate to react version 18.

--- React/investment-builder-client/src/index.js
+++ React/investment-builder-client/src/index.js
@@ -1,15 +1,31 @@
-import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import TopLevel from './TopLevel.js'
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import reportWebVitals from './reportWebVitals';
+// import TopLevel from './TopLevel.js'
 
 
-ReactDOM.render(
-   <TopLevel />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//    <TopLevel />,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import TopLevel from './TopLevel.js'
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <TopLevel />
+  </React.StrictMode>
+);
+
 reportWebVitals();
